1e510fa95c58f24642437fa9f2a81da20e5d87cc
Midhun Kumar Singh Alluru <midhunkumar.alluru@hotmail.com>
2026-05-23 16:35:49 +0530

    fixed

--- octofit-tracker/frontend/src/components/Activities.jsx
+++ octofit-tracker/frontend/src/components/Activities.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ResourcePage from './ResourcePage'
 
 export default function Activities() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api/activities')
+      .then(res => res.json())
+      .then(data => setData(data))
+  }, [])
+
   return (
     <ResourcePage
       resource="activities"
       title="Activities"
       columns={["id", "user", "activityType", "duration", "timestamp"]}
+      data={data}
     />
   )
 }
--- octofit-tracker/frontend/src/components/Leaderboard.jsx
+++ octofit-tracker/frontend/src/components/Leaderboard.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ResourcePage from './ResourcePage'
 
 export default function Leaderboard() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api/leaderboard')
+      .then(res => res.json())
+      .then(data => setData(data))
+  }, [])
+
   return (
     <ResourcePage
       resource="leaderboard"
       title="Leaderboard"
       columns={["id", "user", "score", "rank"]}
+      data={data}
     />
   )
 }
